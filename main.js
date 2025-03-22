36cdd18feea784205568edff73645ea442fc063f
juaquinbab <camilo.jimenez.r@hotmail.com>
2025-03-21 20:21:25 -0500

    commit21821

--- main.js
+++ main.js
@@ -869,73 +869,61 @@ app.post("/webhook", function (request, response) {
   // /////////////////////////////////////////
 
 
+   const folderPath15 = './sala1'; // Carpeta donde se guardarán los archivos JSON
+  const folderPath16 = './sala2';
+  const batchSize = 10;
 
-const folderPath15 = './sala1';
-const folderPath16 = './sala2';
-const folderPath17 = './sala3';
-const batchSize = 10;
+  function archivoExisteEnCarpetas(fileName) {
+    const filePath15 = path.join(folderPath15, fileName);
+    const filePath16 = path.join(folderPath16, fileName);
 
-function archivoExisteEnCarpetas(fileName) {
-  const filePath15 = path.join(folderPath15, fileName);
-  const filePath16 = path.join(folderPath16, fileName);
-  const filePath17 = path.join(folderPath17, fileName);
-
-  if (fs.existsSync(filePath15)) {
-    return { exists: true, filePath: filePath15 };
-  } else if (fs.existsSync(filePath16)) {
-    return { exists: true, filePath: filePath16 };
-  } else if (fs.existsSync(filePath17)) {
-    return { exists: true, filePath: filePath17 };
-  } else {
-    return { exists: false, filePath: null };
-  }
-}
-
-function procesarMensajesBatch15(mensajes) {
-  const mensajesBatch = mensajes.splice(0, batchSize);
-
-  mensajesBatch.forEach((mensaje, index) => {
-    if (mensaje.etapa >= 4 && mensaje.etapa <= 14) {
-      let folderPath;
-      if (index % 3 === 0) {
-        folderPath = folderPath15;
-      } else if (index % 3 === 1) {
-        folderPath = folderPath16;
-      } else {
-        folderPath = folderPath17;
-      }
-
-      const fileName = `${mensaje.from}.json`;
-      const filePath = path.join(folderPath, fileName);
-
-      try {
-        const archivoExistencia = archivoExisteEnCarpetas(fileName);
-
-        if (archivoExistencia.exists) {
-          const existingData = JSON.parse(fs.readFileSync(archivoExistencia.filePath, 'utf8'));
-          const isDuplicateBody = existingData.some((existingMensaje) => existingMensaje.body === mensaje.body);
-
-          if (!isDuplicateBody) {
-            existingData.push(mensaje);
-            fs.writeFileSync(archivoExistencia.filePath, JSON.stringify(existingData, null, 2));
-          }
-        } else {
-          fs.writeFileSync(filePath, JSON.stringify([mensaje], null, 2));
-        }
-      } catch (error) {
-        console.error(`Error procesando el mensaje ${mensaje.from}:`, error);
-      }
+    if (fs.existsSync(filePath15)) {
+      return { exists: true, filePath: filePath15 };
+    } else if (fs.existsSync(filePath16)) {
+      return { exists: true, filePath: filePath16 };
+    } else {
+      return { exists: false, filePath: null };
     }
-  });
-
-  if (mensajes.length > 0) {
-    setTimeout(() => {
-      procesarMensajesBatch15(mensajes);
-    }, 1000);
   }
-}
 
-procesarMensajesBatch15(EtapasMSG.slice());
+  function procesarMensajesBatch15(mensajes) {
+    const mensajesBatch = mensajes.splice(0, batchSize); // Obtener el próximo lote de mensajes
+
+    mensajesBatch.forEach((mensaje, index) => {
+      if (mensaje.etapa >= 4 && mensaje.etapa <= 14) {
+
+        const folderPath = index % 2 === 0 ? folderPath15 : folderPath16;
+        const fileName = `${mensaje.from}.json`;
+        const filePath = path.join(folderPath, fileName);
+
+        try {
+          const archivoExistencia = archivoExisteEnCarpetas(fileName);
+
+          if (archivoExistencia.exists) {
+            const existingData = JSON.parse(fs.readFileSync(archivoExistencia.filePath, 'utf8'));
+            const isDuplicateBody = existingData.some((existingMensaje) => existingMensaje.body === mensaje.body);
+
+            if (!isDuplicateBody) {
+              existingData.push(mensaje);
+              fs.writeFileSync(archivoExistencia.filePath, JSON.stringify(existingData, null, 2));
+            }
+          } else {
+            fs.writeFileSync(filePath, JSON.stringify([mensaje], null, 2));
+          }
+        } catch (error) {
+          console.error(`Error procesando el mensaje ${mensaje.from}:`, error);
+        }
+      }
+    });
+
+    if (mensajes.length > 0) {
+      setTimeout(() => {
+        procesarMensajesBatch15(mensajes);
+      }, 1000);
+    }
+  }
+
+  procesarMensajesBatch15(EtapasMSG.slice());
 
 
 
